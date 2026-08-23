2a9fddc005c91cb39370477e8e0533924d319ba7
thaivanduy001-lab <thaivanduy001@gmail.com>
2026-08-23 11:49:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def run_web():
 t = Thread(target=run_web)
 t.start()
 
-CHANNEL_ID = 1497266748087341076
+CHANNEL_ID = [1497266748087341076,1485274014308892831,1540945642686255104]
 client = discord.Client()
 
 def get_vietnamese_word(start_word):
