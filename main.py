954c6bc933418b4d383a27d949057fa0fb752287
thaivanduy001-lab <thaivanduy001@gmail.com>
2026-08-24 15:41:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ t.daemon = True
 t.start()
 
 # Danh sách ID kênh chạy nối từ
-CHAN_IDS = [1497266748087341076, 1485274014308892831, 1540945642686255104]
+CHANNEL_IDS = [1497266748087341076, 1485274014308892831, 1540945642686255104]
 client = discord.Client()
 
 def get_vietnamese_word(start_word):
